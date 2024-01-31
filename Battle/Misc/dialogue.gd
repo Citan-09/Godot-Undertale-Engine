3f492681c6fd930d3953e3ccf69bf22e661eda2d
Citan <128477870+02IOOO@users.noreply.github.com>
2024-01-31 21:49:11 +0800

    Update dialogue.gd

--- Battle/Misc/dialogue.gd
+++ Battle/Misc/dialogue.gd
@@ -2,7 +2,7 @@ extends Control
 
 @export var custom_character: EnemySpeech.characters
 @onready var bubble_text: EnemySpeech = $TextContainer/Text
-var expression_array := []
+#var expression_array := []
 signal set_head(head_id: int)
 
 func _ready() -> void:
@@ -13,13 +13,15 @@ func _ready() -> void:
 
 ##exp_arr is a nested array where you can get an array of values for each line (useful for sans shrugging and stuff)
 func DialogueText(text: Array, exp_arr: Array):
-	expression_array = exp_arr
+	#expression_array = exp_arr
+	bubble_text.text = ""
 	await create_tween().tween_property(self, "modulate:a", 1, 0.1).finished
 	bubble_text.typetext(text)
+	for i in exp_arr.size():
+		var exp = await bubble_text.startedtyping
+		#_set_expression(exp_arr[exp])
+		set_head.emit(exp_arr[exp])
 	await bubble_text.finishedalltexts
 	create_tween().tween_property(self, "modulate:a", 0, 0.1)
-	bubble_text.text = ""
 
-func _on_text_startedtyping(line: int) -> void:
-	var current_expression = expression_array[line]
-	emit_signal("set_head", current_expression)
+
