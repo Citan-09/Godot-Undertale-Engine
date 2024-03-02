556d982bef1d42615a3cff98c32b84c670507fbb
Citan <128477870+Citan-09@users.noreply.github.com>
2024-03-02 20:10:57 +0800

    Update hud.gd

--- Battle/hud.gd
+++ Battle/hud.gd
@@ -14,13 +14,14 @@ class_name BattleHUD
 func _process(_delta: float) -> void:
 	Name.text = str(Global.player_name)
 	Lv.text = "Lv " + str(Global.player_lv)
-	HpBarContainer.custom_minimum_size.x = min(max(Global.player_max_hp*1.2 +1,4),220)
+	HpBarContainer.custom_minimum_size.x = min(max(Global.player_max_hp*1.2 +1,4), 160)
 	HpBar.value = Global.player_hp - Global.player_kr
 	KrBar.value = Global.player_hp
 	KrBar.max_value = Global.player_max_hp
 	HpBar.max_value = Global.player_max_hp
-	var hptext := "[color=%s]" % Color.MAGENTA.to_html() if Global.player_kr >0 else ""
+	var hptext := "[color=%s]" % Color.MAGENTA.to_html() if Global.player_kr > 0 else ""
 	Hp.text = hptext + "%s / %s" % [Global.player_hp,Global.player_max_hp]
+
 
 func _ready() -> void:
 	_process(0.0)
