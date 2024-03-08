33dec3d60e4758f64be80e1510f49b0cf124a696
Citan <128477870+Citan-09@users.noreply.github.com>
2024-03-08 18:45:14 +0800

    Update defaultbullet.gd

--- Battle/Bullets/defaultbullet.gd
+++ Battle/Bullets/defaultbullet.gd
@@ -22,14 +22,12 @@ func queue_fire(delay: float, target: Vector2, movement_type: int, speed: float 
 
 var tw: Tween
 
-func _await_fire(fire_call: Callable, delay: float) -> DefaultBullet:
+func _await_fire(fire_call: Callable, delay: float):
 	if tw and tw.is_valid():
-		tw.pause()
-		tw.finished.connect(tw.play)
+		await tw.finished
 	tw = create_tween()
 	if velocity_tween and velocity_tween.is_running():
 		tw.pause()
 		velocity_tween.finished.connect(tw.play)
 	tw.tween_interval(delay)
 	tw.tween_callback(fire_call)
-	return self
