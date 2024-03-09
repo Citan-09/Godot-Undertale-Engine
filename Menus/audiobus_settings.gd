6269e1b9958d02ed3180596d3ca1bda5cfbde388
Citan <128477870+Citan@users.noreply.github.com>
2024-03-09 17:08:11 +0800

    Bus

--- Menus/audiobus_settings.gd
+++ Menus/audiobus_settings.gd
@@ -12,15 +12,23 @@ extends Control
 
 func _ready() -> void:
 	Name.text = "[center]" + String(audio_bus_name)
-	VolumeSlider.value = Global.settings.get(String(audio_bus_name), 100)
+	VolumeSlider.set_value_no_signal(Global.settings.get(String(audio_bus_name), 100))
 	
-
+var tw: Tween
 
 func _on_volume_slider_value_changed(value: float) -> void:
-	Info.position = Vector2(VolumeSlider.position.x + VolumeSlider.size.x / 2 + 20, get_local_mouse_position().y - 18)
+	Info.position = Vector2(
+		VolumeSlider.position.x + VolumeSlider.size.x / 2 + 20,
+		lerpf(1 - VolumeSlider.value / VolumeSlider.max_value, 0.5, 0.3) * VolumeSlider.size.y - 10 + VolumeSlider.position.y)
 	AudioServer.set_bus_volume_db(audiobus_id, linear_to_db(value / 100.0))
 	Info.text = "%3.0f db" % [linear_to_db(value / 100.0)]
 	Global.settings[String(audio_bus_name)] = value
+	_on_v_slider_drag_started()
+	if tw and tw.is_valid():
+		tw.kill()
+	tw = create_tween()
+	tw.tween_callback(_on_v_slider_drag_ended).set_delay(0.5)
+	
 
 
 
@@ -28,7 +36,7 @@ func _on_v_slider_drag_started() -> void:
 	Info.show()
 
 
-func _on_v_slider_drag_ended(value_changed: bool) -> void:
+func _on_v_slider_drag_ended() -> void:
 	Info.hide()
 
 
@@ -42,3 +50,4 @@ func _on_mute_toggled(toggled_on: bool) -> void:
 
 func _on_solo_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_solo(audiobus_id, toggled_on)
+
